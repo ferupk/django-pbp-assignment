30c72f81601e30aaf811ba862bda947ddc737d83
ferupk <feru.pratama@ui.ac.id>
2022-09-14 22:10:46 +0700

    katalog setup, MVT implemented

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
+from django.urls import path
+from katalog.views import show_catalog
+
 # TODO: Implement Routings Here
+app_name = 'katalog'
+
+urlpatterns = [
+    path('', show_catalog, name='show_catalog'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    catalog_item_data = CatalogItem.objects.all()
+    context = {
+        'catalog_list': catalog_item_data,
+        'name': "Feru Pratama Kartajaya",
+        'student_id': "2106750351"
+    }
+    return render(request, "katalog.html", context)
