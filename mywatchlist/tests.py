90f0df0a936909c9e30de37c59835d5d6206db2d
ferupk <feru.pratama@ui.ac.id>
2022-09-22 10:48:04 +0700

    added unit tests for mywatchlist views

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve
 
-# Create your tests here.
+class MyWatchListViewsTest(TestCase):
+    def test_html_view(self):
+        response = self.client.get('/mywatchlist/html', follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_xml_view(self):
+        response = self.client.get('/mywatchlist/xml', follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_json_view(self):
+        response = self.client.get('/mywatchlist/json', follow=True)
+        self.assertEqual(response.status_code, 200)
